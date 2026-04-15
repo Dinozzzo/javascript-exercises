0beb823a53c4424df6afa8ac23efa2f2dd59fa70
Dinozzzo <dleootta@gmail.com>
2026-04-15 15:17:40 +0200

    Update contains.js

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,21 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  for (let key in obj) {
+    const current = obj[key];
+    if (current === value) {
+      return true;
+    }
+
+    if (typeof current === "object" && current !== null) {
+      if (contains(current, value)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
+
+console.log(contains({ foo: { bar: "bar" } }, "bar"));
+
   
 // Do not edit below this line
 module.exports = contains;
