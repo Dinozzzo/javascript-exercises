811846536a5a8dae2946ff1ef177f2d874476ac1
Dinozzzo <dleootta@gmail.com>
2026-04-15 15:28:36 +0200

    Update totalIntegers.js

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,16 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data) {
+  let count = 0;
+  for (let key in data) {
+    const value = data[key];
+    if (Number.isInteger(value)) {
+      count++;
+    } else if (Array.isArray(value)) {
+      count += totalIntegers(value);
+    } else if (typeof value === "object" && value !== null) {
+      count += totalIntegers(value);
+    }
+  }
+  return count;
 };
   
 // Do not edit below this line
